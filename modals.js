1cf04db9d0068bebc4e376c2534858439fe72d09
kolya130899 <kolyawote75@gmail.com>
2022-01-22 01:26:37 +0200

    delete unnecessary files

--- modals.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("yeah");
-
-  document.querySelector(".modal-open").addEventListener("click", () => {
-    window.open("modal.htm", "_self");
-  });
-});
